584cc7fc4b4655a094766b381cff6b4103c7c599
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-05-20 03:39:28 -0400

    added public directory

--- pages/Index.js
+++ pages/Index.js
@@ -3,10 +3,15 @@ import Hero from "../components/Hero/Hero";
 import PriceTable from "../components/PriceTable/PriceTable";
 import Controls from "../components/PriceTable/Controls";
 import styles from "./Index.module.css";
+import Head from "next/head";
 
 const LivePrices = (props) => {
   return (
     <Layout>
+      <Head>
+        <title>CoinStats Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <div className={styles.container}>
         <Hero />
         <Controls />
